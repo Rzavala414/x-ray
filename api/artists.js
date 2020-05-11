dfa135a7c7b91c777997ae8140d988ceaec358e9
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-11 10:31:40 -0700

    created new put route

--- api/artists.js
+++ api/artists.js
@@ -27,4 +27,36 @@ artistsRouter.get('/', (req, res, next) =>{
         })
 })
 
+artistsRouter.post('/', (req, res, next) =>{
+
+});
+
+artistsRouter.get('/:artistId', (req, res, next) => {
+    res.status(200).json({artist: req.artist});
+});
+
+artistsRouter.post('/', (req, res, next) =>{
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+    if(!req.body.artists.name || !req.body.artists.dataOfBirth || !req.body.artists.biography ){
+        res.sendStatus(400);
+    }
+    db.run(`INSERT INTO Artists(name, date_of_birth, biography, is_currently_employed) VALUES(${req.body.artist.name}, ${req.body.artists.dateOfBirth},${req.body.artist.biography},${isCurrentlyEmployed})`, (err,artist) =>{
+        res.status(201).json({artist: artist});
+    },function(){
+
+    })
+});
+
+artistsRouter.put('/:artistId', (req, res, next) =>{
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.artist.dateOfBirth;
+    const biography = req.body.artist.biography;
+
+    if (!name || !dateOfBirth || !biography) {
+         return res.sendStatus(400);
+    }
+});
+
+
+
 module.exports = artistsRouter;
