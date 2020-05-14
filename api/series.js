000e45239a1f85fe4d4c9c98974a801733117e9d
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-14 13:20:47 -0700

    created route param for seriesId

--- api/series.js
+++ api/series.js
@@ -3,7 +3,23 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
-seriesRouter.get('/', (req, res, next) => {
+seriesRouter.param('/seriesId', (req, res, next, id) => {
+
+    db.get(`SELECT * FROM Series WHERE id = ${id}`, (error, series) => {
+
+        if(error){
+            next(error);
+        }else if(series){
+            req.series = series;
+            next();
+        }else{
+            res.sendStatus(404);
+        }
+
+    });
+});
+
+seriesRouter.get('/:seriesId', (req, res, next) => {
 
     db.all('SELECT * FROM Series', (error, series) =>{
         if(error){
