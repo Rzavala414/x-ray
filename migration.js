d90f6a7ff138dddd52ed6c520b5d3872388fe90a
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-15 12:37:00 -0700

    created new table

--- migration.js
+++ migration.js
@@ -18,3 +18,14 @@ db.serialize(() => {
         description TEXT NOT NULL
     )`);
 });
+
+db.serialize(() => {
+    db.run(`CREATE TABLE Issue(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        issue_number INTEGER NOT NULL,
+        publication_date TEXT NOT NULL,
+        artist_id INTEGER NOT NULL,
+        series_id INTEGER NOT NULL
+    )`);
+});
