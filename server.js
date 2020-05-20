773be61c94e2e44b6d4e30f8cb58c179164297b3
Roger Zavala <rogeliozavala15@gmail.com>
2020-05-20 14:17:03 -0700

    fixed cors and bodyparser ref error

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const errorhandler = require('errorhandler');
+const cors = require('cors');
 const apiRouter = require('./api/api.js');
+
 const PORT = process.env.PORT || 4001;
 
 
-app.use(app.use(bodyParser.json()));
-app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api', apiRouter);
 
